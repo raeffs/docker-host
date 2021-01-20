1048e2941cb4ed82177f29832a6be300f678efdf
Raphael Fleischlin <git@raeffs.dev>
2021-01-20 19:32:28 +0100

    remove volumes

--- init-workspace.sh
+++ init-workspace.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-mkdir -p .certificates
-touch .certificates/acme.json
-chmod 600 .certificates/acme.json
+mkdir -p .data/certificates
+touch .data/certificates/acme.json
+chmod 600 .data/certificates/acme.json
 
-mkdir -p .log/traefik
-touch .log/traefik/access.log
-mkdir -p .log/gitea
-touch .log/gitea/gitea.log
+mkdir -p .data/log/traefik
+touch .data/log/traefik/access.log
+mkdir -p .data/log/gitea
+touch .data/log/gitea/gitea.log
+
+mkdir -p .data/data/matomo
+chown 1001:1001 .data/data/matomo
