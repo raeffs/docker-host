5741b70f10306527dbd2661345b944428d16fc0b
Raphael Fleischlin <git@raeffs.dev>
2021-04-01 16:44:36 +0200

    remove unused services

--- init-workspace.sh
+++ init-workspace.sh
@@ -4,12 +4,4 @@ mkdir -p .data/certificates
 touch .data/certificates/acme.json
 chmod 600 .data/certificates/acme.json
 
-mkdir -p .data/log/traefik
-touch .data/log/traefik/access.log
-mkdir -p .data/log/gitea
-touch .data/log/gitea/gitea.log
-
-mkdir -p .data/data/matomo
-chown 1001:1001 .data/data/matomo
-
 docker network create --subnet 172.20.0.0/24 --ip-range 172.20.0.128/25 docker-host-network
