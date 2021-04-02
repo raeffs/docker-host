5645df993be47f04416c1fca97c87835347a9593
Raphael Fleischlin <git@raeffs.dev>
2021-04-02 10:56:00 +0200

    fix backup and restore scripts

--- restore-volumes.sh
+++ restore-volumes.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-mkdir -p /tmp/docker-host-backup
-
-docker run -v commento-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup commento-db
-docker run -v gitea:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup gitea
-docker run -v gitea-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup gitea-db
-docker run -v grafana:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup grafana
-docker run -v home-assistant:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup home-assistant
-docker run -v home-assistant-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup home-assistant-db
-docker run -v matomo:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup matomo
-docker run -v matomo-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup matomo-db
-docker run -v pihole:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup pihole
-docker run -v pihole-dnsmasq:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup pihole-dnsmasq
-docker run -v portainer:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup portainer
-docker run -v prometheus:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup prometheus
-docker run -v recipes-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup recipes-db
-docker run -v recipes-media:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup recipes-media
-docker run -v seq:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup backup seq
+docker run -v commento-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore commento-db
+docker run -v gitea:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore gitea
+docker run -v gitea-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore gitea-db
+docker run -v grafana:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore grafana
+docker run -v home-assistant:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore home-assistant
+docker run -v home-assistant-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore home-assistant-db
+docker run -v matomo:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore matomo
+docker run -v matomo-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore matomo-db
+docker run -v pihole:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore pihole
+docker run -v pihole-dnsmasq:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore pihole-dnsmasq
+docker run -v portainer:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore portainer
+docker run -v prometheus:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore prometheus
+docker run -v recipes-db:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore recipes-db
+docker run -v recipes-media:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore recipes-media
+docker run -v seq:/volume -v /tmp/docker-host-backup:/backup --rm loomchild/volume-backup restore seq
