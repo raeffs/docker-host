ccde13c553e178a1a340976c102bb75bb2ece4f9
Raphael Fleischlin <git@raeffs.dev>
2021-04-02 10:45:30 +0200

    remove old init script

--- init-workspace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p .data/certificates
-touch .data/certificates/acme.json
-chmod 600 .data/certificates/acme.json
-
-docker network create --subnet 172.20.0.0/24 --ip-range 172.20.0.128/25 docker-host-network
