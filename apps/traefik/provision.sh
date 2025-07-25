beffb8d52e384bdc8629ca066cdfb15d13728600
Raphael Fleischlin <git@raeffs.dev>
2025-07-25 20:25:59 +0200

    apps(traefik): remove acme email

--- apps/traefik/provision.sh
+++ apps/traefik/provision.sh
@@ -4,7 +4,3 @@
 mkdir -p /certificates
 touch /certificates/acme.json
 chmod 600 /certificates/acme.json
-
-# copy configuration template and replace variables
-cp /traefik.template.yml /etc/traefik/traefik.yml
-sed -i "s/admin@example.com/$ACME_EMAIL/" /etc/traefik/traefik.yml
