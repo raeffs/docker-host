aa8b021aa896782447538153a8a828be28afbf3c
Raphael Fleischlin <git@raeffs.dev>
2025-08-02 08:21:05 +0200

    feature(tools): add option to specify environment name

--- tools/docker-compose/src/utils/get-compose-files.ts
+++ tools/docker-compose/src/utils/get-compose-files.ts
@@ -1,14 +1,11 @@
 import { joinPathFragments } from '@nx/devkit';
-import { isCiEnvironment } from './environment';
+import { getEnvironmentName } from './environment';
 import { existsSync } from 'fs';
 
 export function getComposeFiles(projectRoot: string): string[] {
   const composeFiles = [joinPathFragments(projectRoot, 'docker-compose.yml')];
 
-  const environmentComposeFile = joinPathFragments(
-    projectRoot,
-    isCiEnvironment() ? 'docker-compose.ci.yml' : 'docker-compose.local.yml'
-  );
+  const environmentComposeFile = joinPathFragments(projectRoot, `docker-compose.${getEnvironmentName()}.yml`);
 
   if (existsSync(environmentComposeFile)) {
     composeFiles.push(environmentComposeFile);
